2aedad49bbbed27880bc8c4850ad54903eccb78a
ebrahim <eb.kh1372@gmil.com>
2023-08-13 23:24:03 +0330

    add simple useState to ExpensesItem

--- src/components/Expenses/ExpenseItems.jsx
+++ src/components/Expenses/ExpenseItems.jsx
@@ -1,16 +1,19 @@
+import { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItems.css'
 
 function ExpenseItems(props){
-    
-  const clickHandler=()=>{console.log("clicked");}
+   const [title,setTitle] = useState(props.title) 
+  const clickHandler=()=>{
+    setTitle('updated')
+    console.log("clicked");}
     return(
         <div className='expense-item'>
           <div>  
             <ExpenseDate date={props.date} /> 
           </div>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
             </div>
             <div className='expense-item__price' >{props.amount} </div>
             <button onClick={clickHandler}> change</button>
