dd1a0dd318dc9ef4dfea11871c27f60bffb71b4d
ebrahim <eb.kh1372@gmil.com>
2023-08-13 23:42:46 +0330

    add NewExpense components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from './components/Expenses/Expenses'
+import NewExpense from './components/NewExpense/NewExpense'
 // import './App.css' // Import the external CSS file
 
 const App = () => {
@@ -26,7 +27,7 @@ const App = () => {
 
   return (
     <div>
-      hello world
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   )
